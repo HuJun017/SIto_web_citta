a8bca2ffa020e2185d262c81af2be348b7f5b998
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-12-02 12:51:46 +0000

    sistemazioni finali

--- app.py
+++ app.py
@@ -2,18 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#route homepage
 @app.route("/")
 def homepage():
     return render_template('homepage.html')
 
+#route per la pagina di storia
 @app.route('/storia')
 def storia():
     return render_template('storia.html')
 
+#route per la pagina dei Luoghi
 @app.route('/luoghi')
 def luoghi():
     return render_template('luoghi.html')
 
+#route per la pagina delle curiosità
 @app.route('/curiosita')
 def curiosita():
     return render_template('curiosita.html')
